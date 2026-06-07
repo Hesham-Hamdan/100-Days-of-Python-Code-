b97ad84ba2dd07656b05934923635a3d343ecdf8
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-06-07 22:50:19 +0300

    feat: add bids comparison and declaring the winner

--- Day 9/main.py
+++ Day 9/main.py
@@ -7,3 +7,13 @@ while continue_bidding == "yes":
     all_bids[name] = bid
     continue_bidding = input("Are there any other bidders? Type 'yes' or 'no' \n")
     print("\n" * 20)
+
+bigger_bid = ["", 0]
+
+for bidder in all_bids:
+    if all_bids[bidder] > bigger_bid[1]:
+        bigger_bid[1] = all_bids[bidder]
+        bigger_bid[0] = bidder
+
+
+print(f"The winner is {bigger_bid[0]} with a bid of {bigger_bid[1]}")
