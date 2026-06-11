8b99c8503293bd0b04ff36ff77a437188f9a52d3
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-06-11 21:01:23 +0300

    feat: add two helping functions

--- Day 14/lower_higher_game.py
+++ Day 14/lower_higher_game.py
@@ -10,3 +10,18 @@ def printing(letter, character):
     print(
         f"{letter}: {character['name']}, a {character['description']}, from {character['country']}."
     )
+
+
+def check_answer(guess, num1, num2):
+    win = max(num1, num2)
+    if guess["follower_count"] == win:
+        return True
+    else:
+        return False
+
+
+def choosing_characters():
+    characters = {"a": shuffle(), "b": shuffle()}
+    while characters["a"] == characters["b"]:
+        characters["b"] = shuffle()
+    return characters
