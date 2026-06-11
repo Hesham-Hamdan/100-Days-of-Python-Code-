2fc7d97ad192801ccdb7fe36fa936cab63679316
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-06-11 21:01:54 +0300

    feat: add playing functionality

--- Day 14/lower_higher_game.py
+++ Day 14/lower_higher_game.py
@@ -25,3 +25,38 @@ def choosing_characters():
     while characters["a"] == characters["b"]:
         characters["b"] = shuffle()
     return characters
+
+
+def playing():
+    continue_playing = True
+    score = 0
+    prev = {}
+    while continue_playing:
+        accounts = choosing_characters()
+
+        if prev != {}:
+            accounts["a"] = prev
+
+        printing("Compare A", accounts["a"])
+        printing("Against B", accounts["b"])
+
+        account1_followers = accounts["a"]["follower_count"]
+        account2_followers = accounts["b"]["follower_count"]
+
+        guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+        print("\n" * 10)
+        is_correct = check_answer(
+            accounts[guess], account1_followers, account2_followers
+        )
+
+        if is_correct:
+            score += 1
+            prev = accounts["b"]
+            print(f"You're right! Current score: {score}")
+        else:
+            print(f"Sorry, that's wrong. Final score: {score}")
+            continue_playing = False
+
+
+playing()
