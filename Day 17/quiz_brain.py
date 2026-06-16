cd6eb9f41e2f0328cf2c069f569d83d09702cfdb
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-06-16 23:10:28 +0300

    feat: add method to move to next question

--- Day 17/quiz_brain.py
+++ Day 17/quiz_brain.py
@@ -12,3 +12,11 @@ class QuizBrain:
             print("wrong answer")
         print(f"The correct answer was {correct_answer}")
         print(f"Your current score is {self.user_score} / {self.question_num}")
+
+    def next_question(self):
+        question = self.question_list[self.question_num]
+        self.question_num += 1
+        user_answer = input(
+            f"Q:{self.question_num} {question.text}: (True/False): "
+        ).lower()
+        self.check_answer(user_answer, question.answer.lower())
