4528f556dc529e07a59679dc78bce66c04ba19f6
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-06-16 23:09:42 +0300

    feat: add method to check the answer

--- Day 17/quiz_brain.py
+++ Day 17/quiz_brain.py
@@ -3,3 +3,12 @@ class QuizBrain:
         self.question_num = 0
         self.question_list = list
         self.user_score = 0
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer == correct_answer:
+            print("You got it right")
+            self.user_score += 1
+        else:
+            print("wrong answer")
+        print(f"The correct answer was {correct_answer}")
+        print(f"Your current score is {self.user_score} / {self.question_num}")
