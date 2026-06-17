e4e33c37c1f2d53571fba5492fcf9c27620e449f
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-06-17 22:31:57 +0300

    feat: add solution to challenge #4

--- Day 18/challenges.py
+++ Day 18/challenges.py
@@ -3,10 +3,10 @@ import random
 
 
 t = Turtle()
+# t.shape("turtle")
 
 
 # challenge 3
-
 sides = 3
 
 colours = [
@@ -20,12 +20,24 @@ colours = [
     "SeaGreen",
 ]
 
-for num in range(3, 11):
+# for num in range(3, 11):
+#     t.pencolor(random.choice(colours))
+#     for num in range(0, sides):
+#         t.forward(100)
+#         t.right(360 / sides)
+#     sides += 1
+
+
+# challenge 4
+
+
+angles = [0, 90, 180, 270, 360]
+t.speed("fastest")
+for num in range(0, 100):
     t.pencolor(random.choice(colours))
-    for num in range(0, sides):
-        t.forward(100)
-        t.right(360 / sides)
-    sides += 1
+    t.right(random.choice(angles))
+    t.forward(20)
+    t.pensize(t.pensize() + 0.1)
 
 
 screen = Screen()
