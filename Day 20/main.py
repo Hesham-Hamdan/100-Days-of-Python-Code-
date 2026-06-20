557a937692d0a8439a7930d4884eb06265c764b5
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-06-21 00:08:35 +0300

    feat: add eating functionality

--- Day 20/main.py
+++ Day 20/main.py
@@ -1,6 +1,7 @@
 import time
 from turtle import Screen
 from snake import Snake
+from food import Food
 
 
 screen = Screen()
@@ -10,6 +11,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 
 screen.listen()
@@ -23,6 +25,10 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
 
 
 screen.exitonclick()
