f059b8c301d6de880c3d9c6f2321ed3800f2741d
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-06-20 23:55:00 +0300

    first commit

---
+++ Day 20/main.py
@@ -0,0 +1,11 @@
+from turtle import Screen
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+
+screen.exitonclick()
