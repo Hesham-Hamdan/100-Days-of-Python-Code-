cd9681ad86a0b7b7b1ca5cdbd59f08333b1556c5
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-06-22 20:04:22 +0300

    feat: add the effect of colliding with the snake tail

--- Day 20/main.py
+++ Day 20/main.py
@@ -39,5 +39,10 @@ while game_is_on:
         scoreboard.game_over()
         game_is_on = False
 
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.game_over()
+            game_is_on = False
+
 
 screen.exitonclick()
