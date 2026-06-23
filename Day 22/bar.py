fdb3462723b83c805a75b4647b25b0c315be715c
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-06-23 23:52:39 +0300

    first commit

---
+++ Day 22/bar.py
@@ -0,0 +1,13 @@
+from turtle import Turtle
+
+
+class Bar(Turtle):
+    def __init__(self, position_x):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.shapesize(1, 5)
+        self.goto(position_x, 0)
+        self.speed("fastest")
+        self.setheading(90)
