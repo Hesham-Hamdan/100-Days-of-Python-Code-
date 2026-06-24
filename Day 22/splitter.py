559d098a09c90e4992593391f6233df11fb59952
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-06-24 22:22:16 +0300

    feat: add method to draw the dash line

--- Day 22/splitter.py
+++ Day 22/splitter.py
@@ -8,3 +8,11 @@ class Splitter(Turtle):
         self.goto(0, -280)
         self.seth(90)
         self.pencolor("white")
+        self.draw()
+
+    def draw(self):
+        for num in range(0, 100):
+            self.pendown()
+            self.forward(10)
+            self.penup()
+            self.forward(10)
