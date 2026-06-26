0791db55188a3bc49140c00523fabb06b5073c00
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-06-26 23:11:23 +0300

    feat: add method to move

--- Day 22/ball.py
+++ Day 22/ball.py
@@ -16,3 +16,8 @@ class Ball(Turtle):
         self.x_move = 20
         self.y_move = 20
         self.move_speed = 0.1
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
