910b1c05276c46be74e90df89e4d06b850249295
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-06-28 22:17:12 +0300

    feat: add method to update the score

--- Day 22/scoreboard.py
+++ Day 22/scoreboard.py
@@ -11,3 +11,8 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(x_postion, 260)
         self.hideturtle()
+        self.update_score(0)
+
+    def update_score(self, score):
+        self.clear()
+        self.write(f"{score}", align=ALIGNMENT, font=FONT)
