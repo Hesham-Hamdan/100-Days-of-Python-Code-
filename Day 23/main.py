68db435a9f526f7f94d9104a39cf3903b7b6c095
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-06-30 22:55:32 +0300

    feat: add cars and thier moving functionality

--- Day 23/main.py
+++ Day 23/main.py
@@ -1,8 +1,31 @@
+import time
 from turtle import Screen
-
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+screen.listen()
+
+cars = []
+
+for num in range(0, 25):
+    car = CarManager()
+    cars.append(car)
+
+
+screen.onkey(key="Up", fun=player.up)
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    for car in cars:
+        car.move()
 
 
 screen.exitonclick()
