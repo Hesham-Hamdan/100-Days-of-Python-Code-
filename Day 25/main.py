7dd0bc086590f25b2b783d777e27d6b4f4acb7b1
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-07-08 22:48:13 +0300

    feat: add challenge 2 solution

--- Day 25/main.py
+++ Day 25/main.py
@@ -1,13 +1,53 @@
+# with open("Day 25\weather_data.csv") as file:
+#     data = file.readlines()
+
+
 # challenge 1
 
-import csv
+# import csv
 
-with open("Day 25\weather_data.csv") as file:
-    data = csv.reader(file)
-    temps = []
-    for row in data:
-        temp = row[1]
-        if temp != "temp":
-            temps.append(int(temp))
+# with open("Day 25\weather_data.csv") as file:
+#     data = csv.reader(file)
+#     temps = []
+#     for row in data:
+#         temp = row[1]
+#         if temp != "temp":
+#             temps.append(int(temp))
 
-print(temps)
+# print(temps)
+
+# challenge 2
+
+import pandas
+
+data = pandas.read_csv("Day 25\weather_data.csv")
+temps = data["temp"]
+
+avg = sum(temps) / len(temps)
+print(avg)
+
+maximum = temps.max()
+print(maximum)
+
+
+print(data[data["temp"] == maximum])
+
+monday_temp = data[data["day"] == "Monday"]["temp"][0]
+
+feh = monday_temp * 9 / 5 + 32
+print(feh)
+
+# challenge 3
+
+# data = pandas.read_csv("Day 25\Central-Park-Squirrel-Census-Squirrel-Data.csv")
+# squirrels = {
+#     "Fur Color": ["grey", "red", "black"],
+#     "Count": [
+#         len(data[data["Primary Fur Color"] == "Gray"]),
+#         len(data[data["Primary Fur Color"] == "Cinnamon"]),
+#         len(data[data["Primary Fur Color"] == "Black"]),
+#     ],
+# }
+
+# data2 = pandas.DataFrame(squirrels)
+# data2.to_csv("Day 25\squirrel_count.csv")
