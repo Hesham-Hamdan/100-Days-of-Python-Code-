dc96f091261a1dc617c3a4a4138f8a471df7a8f0
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-07-09 23:03:42 +0300

    feat: add starting the game functionality

--- Day 25/states game/main.py
+++ Day 25/states game/main.py
@@ -1,7 +1,25 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "Day 25\states game\states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+
+
+data = pandas.read_csv("Day 25/states game/50_states.csv")
+
+
+title = "Guess the state"
+game_is_on = True
+guessed_states = []
+
+while game_is_on:
+    answer_state = screen.textinput(
+        title=title, prompt="What's another state's name? "
+    ).title()
+
+# for state in data["state"].to_list():
+#     if state not in guessed_states:
+#         not_guessed["state"].append(state)
