cf3b32c363aecb3f8772521a27104fc8aa518bb6
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-07-10 23:08:31 +0300

    feat: add solution to dictionary assignment #2

--- Day 26/main.py
+++ Day 26/main.py
@@ -18,7 +18,22 @@
 # dict assignments
 
 # Assignment 1
-sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
-words = sentence.split(" ")
-result = {word: len(word) for word in words}
-print(result)
+# sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# words = sentence.split(" ")
+# result = {word: len(word) for word in words}
+# print(result)
+
+# Assignment 2
+
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+
+weather_f = {day: (temp * 9 / 5 + 32) for (day, temp) in weather_c.items()}
+print(weather_f)
