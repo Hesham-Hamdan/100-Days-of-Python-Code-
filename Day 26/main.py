58c4aef9fbaff6dcec6933b11cfe402b7c7cf71c
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-07-10 23:07:24 +0300

    feat: add solution to list assignment #2

--- Day 26/main.py
+++ Day 26/main.py
@@ -1,6 +1,15 @@
 # list assignments
 # Assignment 1
 
-mbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-squares = [mber**2 for mber in mbers]
-print(squares)
+# mbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# squares = [mber**2 for mber in mbers]
+# print(squares)
+
+# Assignment 2
+
+list_of_strings = ["9", "0", "32", "8", "2", "8", "64", "29", "42", "99"]
+nums = [int(num) for num in list_of_strings]
+even_nums = [num for num in nums if num % 2 == 0]
+
+print(nums)
+print(even_nums)
