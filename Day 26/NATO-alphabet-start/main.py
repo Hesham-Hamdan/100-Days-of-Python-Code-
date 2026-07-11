facd78969ca5d89efdcee09dd3f449444cc69c91
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-07-11 23:47:07 +0300

    feat: add step 2 of the solution

--- Day 26/NATO-alphabet-start/main.py
+++ Day 26/NATO-alphabet-start/main.py
@@ -13,3 +13,7 @@ data = pandas.read_csv("Day 26/NATO-alphabet-start/nato_phonetic_alphabet.csv")
 # step 1
 letter_word = {row.letter: row.code for (_, row) in data.iterrows()}
 print(letter_word)
+# step 2
+input_word = list(input("Enter a word: ").upper())
+phonetis_words = [letter_word[letter] for letter in input_word]
+print(phonetis_words)
