59da1b7ff08dd8ec2ececa631eac73c2168c0bc5
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-07-13 23:56:42 +0300

    feat: add the counting down functionality

--- Day 28/main.py
+++ Day 28/main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -10,6 +10,8 @@ FONT_NAME = "Courier"
 WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
@@ -18,6 +20,24 @@ LONG_BREAK_MIN = 20
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps / 2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_marks.config(text=marks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -40,10 +60,7 @@ canvas.grid(column=1, row=1)
 start_button = Button(text="Start", highlightthickness=0)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(
-    text="Reset",
-    highlightthickness=0,
-)
+reset_button = Button(text="Reset", highlightthickness=0)
 reset_button.grid(column=2, row=2)
 
 check_marks = Label(fg=GREEN, bg=YELLOW)
