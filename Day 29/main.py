2622b2641ab129a0e7d6ba7b808cc50b47ec1ed0
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-07-14 21:15:30 +0300

    feat: add the rest of the GUI components

--- Day 29/main.py
+++ Day 29/main.py
@@ -77,4 +77,30 @@ canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
 
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+
+
+email_username_label = Label(text="Email/Username:")
+email_username_label.grid(row=2, column=0)
+
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+website = Entry(width=35)
+website.grid(row=1, column=1, columnspan=2)
+
+
+email_username = Entry(width=35)
+email_username.grid(row=2, column=1, columnspan=2)
+
+password = Entry(width=21)
+password.grid(row=3, column=1)
+
+generate_password = Button(text="Generate Password")
+generate_password.grid(row=3, column=2)
+
+add = Button(width=36, text="Add")
+add.grid(row=4, column=1, columnspan=2)
+
 window.mainloop()
