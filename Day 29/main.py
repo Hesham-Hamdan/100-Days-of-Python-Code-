9c42f08e379ea6b5156efa00d210ca494ef69ed4
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-07-14 21:13:34 +0300

    feat: add some of the GUI components

--- Day 29/main.py
+++ Day 29/main.py
@@ -69,5 +69,12 @@ window = Tk()
 window.title("Password Manger")
 window.config(padx=50, pady=50)
 
+canvas = Canvas(width=200, height=200)
+logo_img = PhotoImage(
+    file="Day 29/logo.png",
+)
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
+
 
 window.mainloop()
