274797a9f5208d0f6ee5978488667ec9d10daedb
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-07-16 23:33:49 +0300

    feat: add step 2

--- Day 30/main.py
+++ Day 30/main.py
@@ -6,3 +6,17 @@ phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+
+def output():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        output()
+    else:
+        print(output_list)
+
+
+output()
