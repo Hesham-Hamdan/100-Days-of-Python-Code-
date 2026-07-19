9e8a4d52fc5aa6582a08ddb81f3a43807988fd0c
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-07-19 23:11:45 +0300

    feat: complete the GUI

--- Day 31/main.py
+++ Day 31/main.py
@@ -7,6 +7,10 @@ current_card = {}
 to_learn = {}
 
 
+data = pandas.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+
+
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
@@ -20,6 +24,14 @@ card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
+
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image, highlightthickness=0)
+unknown_button.grid(row=1, column=0)
+
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image, highlightthickness=0)
+known_button.grid(row=1, column=1)
 
 
 window.mainloop()
