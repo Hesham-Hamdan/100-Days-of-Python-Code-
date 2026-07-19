594b75c22fca04c5f281e4ded16c7053a30d2c70
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-07-19 23:08:06 +0300

    feat: add some GUI components

--- Day 31/main.py
+++ Day 31/main.py
@@ -12,4 +12,14 @@ window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+
+
 window.mainloop()
