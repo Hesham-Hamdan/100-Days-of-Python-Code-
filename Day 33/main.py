2e2a33ef860eedc940fa07bdc50300278e5fa816
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-07-21 11:36:03 +0300

    feat: add the location of the space station

--- Day 33/main.py
+++ Day 33/main.py
@@ -5,3 +5,7 @@ iss_response = requests.get(url="http://api.open-notify.org/iss-now.json")
 iss_response.raise_for_status()
 
 iss_data = iss_response.json()
+
+
+longitude = float(iss_data["iss_position"]["longitude"])
+latitude = float(iss_data["iss_position"]["latitude"])
