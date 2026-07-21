4238aadaf1a5cfd6687af98f085019c4397d7a8b
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-07-21 11:37:35 +0300

    feat: add the sunrise and sunset hours at my city

--- Day 33/main.py
+++ Day 33/main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import requests
 
 
@@ -9,3 +10,19 @@ iss_data = iss_response.json()
 
 longitude = float(iss_data["iss_position"]["longitude"])
 latitude = float(iss_data["iss_position"]["latitude"])
+
+
+MY_LAT = 30.725775
+MY_LONG = 31.798065
+
+parameters = {"lat": MY_LAT, "lng": MY_LONG, "formatted": 0}
+
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+print(sunset)
+print(sunrise)
