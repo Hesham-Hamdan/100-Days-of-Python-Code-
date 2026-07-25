d5aa90cd9b8e41070aa71710b181844d0213ca89
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-07-25 17:49:23 +0300

    fix: next question method returns the next question without check

--- Day 34/quiz_brain.py
+++ Day 34/quiz_brain.py
@@ -1,25 +1,29 @@
+import html
+
+
 class QuizBrain:
-    def __init__(self, list):
-        self.question_num = 0
-        self.question_list = list
-        self.user_score = 0
 
-    def check_answer(self, user_answer, correct_answer):
-        if user_answer == correct_answer:
-            print("You got it right")
-            self.user_score += 1
-        else:
-            print("wrong answer")
-        print(f"The correct answer was {correct_answer}")
-        print(f"Your current score is {self.user_score} / {self.question_num}")
-
-    def next_question(self):
-        question = self.question_list[self.question_num]
-        self.question_num += 1
-        user_answer = input(
-            f"Q:{self.question_num} {question.text}: (True/False): "
-        ).lower()
-        self.check_answer(user_answer, question.answer.lower())
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.score = 0
+        self.question_list = q_list
+        self.current_question = None
 
     def still_has_questions(self):
-        return self.question_num < len(self.question_list)
+        return self.question_number < len(self.question_list)
+
+    def next_question(self):
+        self.current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        q_text = html.unescape(self.current_question.text)
+        return f"Q.{self.question_number}: {q_text} (True/False): "
+        # user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
+        # self.check_answer(user_answer)
+
+    def check_answer(self, user_answer):
+        correct_answer = self.current_question.answer
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            return True
+        else:
+            return False
