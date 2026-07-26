a74815be55fbd4f2b3ef39a2e1c156dec2e3d522
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-07-26 19:04:44 +0300

    feat: add some UI components

--- Day 34/ui.py
+++ Day 34/ui.py
@@ -9,3 +9,19 @@ class QuizzInterface:
         self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+        self.scoreboard = Label(text="Score: 0", bg=THEME_COLOR, fg="white")
+        self.scoreboard.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white", highlightthickness=0)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+        self.text = self.canvas.create_text(
+            150,
+            125,
+            width=280,
+            text="Amazon accquired Twitch in August",
+            font=("Ariel", 15, "italic"),
+            fill=THEME_COLOR,
+        )
+
+        self.window.mainloop()
