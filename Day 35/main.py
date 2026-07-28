5bd0a745a380a379a92b1049ad8da3fb9477536c
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-07-28 23:28:24 +0300

    first commit

---
+++ Day 35/main.py
@@ -0,0 +1,6 @@
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = "__YOUR_OWM_API_KEY__"
+account_sid = "__YOUR_TWILIO_ACCOUNT_ID__"
+auth_token = "__YOUR_TWILIO_AUTH_TOKEN__"
+
+
