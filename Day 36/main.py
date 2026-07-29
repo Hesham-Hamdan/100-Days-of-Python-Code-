9ed4dd4581dffa93e278f28826c54ccb534ae618
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-07-29 23:34:18 +0300

    first commit

---
+++ Day 36/main.py
@@ -0,0 +1,9 @@
+STOCK = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+NEWS_API_KEY = "afa580b6630f491d833e03fb936a5f94"
+STOCK_API_KEY = "J16XZTTXL3N4VP2R"
+
+NEWS_API = "https://newsapi.org/v2/everything"
+STOCK_PRICE_API = "https://www.alphavantage.co/query"
+
