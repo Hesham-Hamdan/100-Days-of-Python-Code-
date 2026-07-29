6b5c44a95780d8b9c3429d9a44a8afd6d1bd5cd1
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-07-29 23:36:59 +0300

    feat: add final step instructions

--- Day 36/main.py
+++ Day 36/main.py
@@ -56,3 +56,17 @@ if difference_precentage <= -5 or difference_precentage >= 5:
     )
     print(message.status)
 
+## STEP 3: Use https://www.twilio.com
+# Send a seperate message with the percentage change and each article's title and description to your phone number.
+
+
+# Optional: Format the SMS message like this:
+"""
+TSLA: 🔺2%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+or
+"TSLA: 🔻5%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+"""
