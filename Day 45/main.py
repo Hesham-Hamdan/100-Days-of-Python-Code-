675f4a9b4c6a144510337b33ddc97c70296d9be7
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-08-03 22:03:11 +0300

    feat: extract every link in the page

--- Day 45/main.py
+++ Day 45/main.py
@@ -1,9 +1,20 @@
 from bs4 import BeautifulSoup
+import requests
 
 
-with open("Day 45\website.html") as file:
-    contents = file.read()
+# with open("Day 45\website.html") as file:
+#     contents = file.read()
 
-soup = BeautifulSoup(contents, "html.parser")
+# soup = BeautifulSoup(contents, "html.parser")
 
-print(soup.title)
+# print(soup.title)
+
+
+response = requests.get("https://news.ycombinator.com/news")
+yc_webpage = response.text
+
+soup = BeautifulSoup(yc_webpage, "html.parser")
+anchors = soup.select(".titleline a")
+# print(anchors)
+for anchor in anchors:
+    print(anchor.getText())
