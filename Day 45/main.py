5162375d5a0c652412020c8379fcd024aa444ef7
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-08-04 23:01:06 +0300

    feat: extract every movie in the page

--- Day 45/main.py
+++ Day 45/main.py
@@ -10,11 +10,34 @@ import requests
 # print(soup.title)
 
 
-response = requests.get("https://news.ycombinator.com/news")
-yc_webpage = response.text
+# response = requests.get("https://news.ycombinator.com/news")
+# yc_webpage = response.text
 
-soup = BeautifulSoup(yc_webpage, "html.parser")
-anchors = soup.select(".titleline a")
-# print(anchors)
-for anchor in anchors:
-    print(anchor.getText())
+# soup = BeautifulSoup(yc_webpage, "html.parser")
+# anchors = soup.select(".titleline a")
+# # print(anchors)
+# for anchor in anchors:
+#     print(anchor.getText())
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+}
+
+response = requests.get(
+    "https://empireonline.com/movies/features/best-movies-2", headers=headers
+)
+empire_webpage = response.text
+
+soup = BeautifulSoup(empire_webpage, "html.parser")
+titles = soup.select(".content_content__i0P3p h2 strong")
+
+names = []
+for title in titles:
+    text = title.getText()
+    if ")" in text:
+        names.append(text)
+
+names.reverse()
+with open("Day 45\movies.txt", "w") as file:
+    for name in names:
+        file.write(f"{name}\n")
