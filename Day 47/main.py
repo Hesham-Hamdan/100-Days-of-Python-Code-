e01e4c93a0faf6af4315682842642e3147fa3718
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-08-05 22:31:57 +0300

    first commit

---
+++ Day 47/main.py
@@ -0,0 +1,8 @@
+import requests
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+}
+
+response = requests.get("https://appbrewery.github.io/instant_pot/", headers=headers)
+empire_webpage = response.text
