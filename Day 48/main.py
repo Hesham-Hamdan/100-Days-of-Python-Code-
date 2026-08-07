c93ff0b8a53f61a8f8b3210e3cd90e9d05647e06
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-08-07 23:35:39 +0300

    feat: add the ordered list of the events times

--- Day 48/main.py
+++ Day 48/main.py
@@ -12,3 +12,19 @@ driver.get("https://www.python.org/")
 lis = driver.find_element(
     By.XPATH, '//*[@id="content"]/div/section/div[2]/div[2]/div/ul'
 ).find_elements(By.TAG_NAME, "li")
+
+times = []
+events = []
+for li in lis:
+    times.append(
+        li.find_element(By.TAG_NAME, "time").get_attribute("datetime").split("T")[0]
+    )
+    events.append(li.find_element(By.TAG_NAME, "a").text)
+
+result = {
+    index: {"time": times[index], "name": events[index]}
+    for index in range(0, len(times))
+}
+
+print(result)
+driver.quit()
