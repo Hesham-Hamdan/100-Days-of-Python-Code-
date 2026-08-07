34dcb604d70559fd817ba0d081cef1e4085cbb78
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-08-07 23:34:29 +0300

    feat: extract the events time elements

--- Day 48/main.py
+++ Day 48/main.py
@@ -6,4 +6,9 @@ from time import sleep, time
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.python.org/")
 
+lis = driver.find_element(
+    By.XPATH, '//*[@id="content"]/div/section/div[2]/div[2]/div/ul'
+).find_elements(By.TAG_NAME, "li")
