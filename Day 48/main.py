f68dae1d1144a4d1d62e9bff2d13a42a4bf649c9
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-08-07 23:15:05 +0300

    first commit

---
+++ Day 48/main.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep, time
+
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+
