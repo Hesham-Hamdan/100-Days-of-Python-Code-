41b6b6e354c050a43cee1a8e301d74800679e175
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-08-09 22:35:55 +0300

    first commit

---
+++ Day 49/main.py
@@ -0,0 +1,3 @@
+ACCOUNT_EMAIL = "hossam@gmail.com"  # The email you registered with
+ACCOUNT_PASSWORD = "hossam@gmail.com"  # The password you used during registration
+GYM_URL = "https://appbrewery.github.io/gym/"
