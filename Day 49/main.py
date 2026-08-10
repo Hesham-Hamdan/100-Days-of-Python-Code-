3ed50d834ec760a7b7aaaa341e219671580a9b3f
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2026-08-10 22:00:24 +0300

    feat: complete step 2

--- Day 49/main.py
+++ Day 49/main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import os
 
 
@@ -15,3 +16,14 @@ chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(GYM_URL)
+
+# step 2
+
+driver.implicitly_wait(2)
+
+login_button = driver.find_element(By.ID, "login-button")
+login_button.click()
+email_field = driver.find_element(By.ID, "email-input")
+email_field.send_keys(ACCOUNT_EMAIL)
+password_field = driver.find_element(By.ID, "password-input")
+password_field.send_keys(ACCOUNT_PASSWORD, Keys.ENTER)
